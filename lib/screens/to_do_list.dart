d4117dfe206becc4fbc084f804a80743901e3c9f
Amit Kumar Das <amit.inverse@gmail.com>
2023-05-07 17:55:55 +0530

    ...

--- lib/screens/to_do_list.dart
+++ lib/screens/to_do_list.dart
@@ -1,7 +1,32 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
-class ToDoListScreen extends StatelessWidget {
+class ToDoListScreen extends StatefulWidget {
   const ToDoListScreen({Key? key}) : super(key: key);
+
+  @override
+  _ToDoListScreenState createState() => _ToDoListScreenState();
+}
+
+class _ToDoListScreenState extends State<ToDoListScreen> {
+  late List<Map<String, dynamic>> _toDoItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    final data = await rootBundle.loadString('assets/todo_data.json');
+    if (data != null) {
+      final items = json.decode(data) as List<dynamic>;
+      setState(() {
+        _toDoItems = items.cast<Map<String, dynamic>>();
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,25 +35,28 @@ class ToDoListScreen extends StatelessWidget {
         title: const Text('To-Do List'),
       ),
       body: ListView.builder(
-        itemCount: toDoItems.length,
+        itemCount: _toDoItems.length,
         itemBuilder: (context, index) {
-          final toDoItem = toDoItems[index];
+          final toDoItem = _toDoItems[index];
           return Dismissible(
-            key: ValueKey(toDoItem),
+            key: ValueKey(toDoItem['id']),
             background: Container(
               color: Colors.red,
               alignment: Alignment.centerRight,
               child: const Icon(Icons.delete, color: Colors.white),
             ),
             onDismissed: (direction) {
-              // Remove the item from the data source.
-              toDoItems.removeAt(index);
+              setState(() {
+                _toDoItems.removeAt(index);
+              });
             },
             child: CheckboxListTile(
-              title: Text(toDoItem),
-              value: false,
+              title: Text(toDoItem['task'] ?? ''),
+              value: toDoItem['completed'] ?? false,
               onChanged: (value) {
-                // Update the item's state.
+                setState(() {
+                  _toDoItems[index]['completed'] = value;
+                });
               },
             ),
           );
@@ -43,10 +71,3 @@ class ToDoListScreen extends StatelessWidget {
     );
   }
 }
-
-final toDoItems = [
-  'Buy groceries',
-  'Do laundry',
-  'Finish homework',
-  'Take out the trash',
-];
